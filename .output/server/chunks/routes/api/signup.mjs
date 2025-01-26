cec18bd11412d24bdf3072c3919c73e5dd5ca8f8
Nûr <djedidinur@gmail.com>
2025-01-26 16:16:06 +0100

    release

--- .output/server/chunks/routes/api/signup.mjs
+++ .output/server/chunks/routes/api/signup.mjs
@@ -18,7 +18,7 @@ const signup = defineEventHandler(async (event) => {
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB.NAME
+    database: process.env.DB_NAME
   });
   try {
     await connection.execute(
--- server/api/signup.js
+++ server/api/signup.js
@@ -15,7 +15,7 @@ export default defineEventHandler(async (event) => {
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB.NAME
+    database: process.env.DB_NAME
   });
 
   try {
